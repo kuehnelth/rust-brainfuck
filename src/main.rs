c86cbe20e7d07105fee7929bae64154d1bd6adc7
Thomas Kühnel <kuehnelth@gmail.com>
2019-06-04 21:24:29 +0200

    move tests to lib

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,3 @@ fn main() {
     let mut state = State::new();
     state.execute(&program, &mut std::io::stdin(), &mut std::io::stdout());
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_cat_a() {
-        let mut output = Vec::new();
-        let mut input = "A".as_bytes();
-        let program = " <,.".parse().unwrap();
-        let mut state = State::new();
-        state.execute(&program, &mut input, &mut output);
-        assert_eq!("A", std::str::from_utf8(&output).unwrap());
-    }
-
-    #[test]
-    fn test_a() {
-        let mut output = Vec::new();
-        let program = "++++++++[>++++++++<-]>+.".parse().unwrap();
-        let mut state = State::new();
-        state.execute(&program, &mut std::io::empty(), &mut output);
-        assert_eq!("A", std::str::from_utf8(&output).unwrap());
-    }
-
-    #[test]
-    fn test_hello_world() {
-        let mut output = Vec::new();
-        let program = "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-."
-            .parse()
-            .unwrap();
-        let mut state = State::new();
-        state.execute(&program, &mut std::io::empty(), &mut output);
-        assert_eq!("hello world", std::str::from_utf8(&output).unwrap());
-    }
-}
